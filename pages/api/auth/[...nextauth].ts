b4d0e40b8a9e6bfc482803a04cf58da32888567a
Minhaj Abdul Rahim <minhajabdulraheem@gmail.com>
2023-03-15 00:23:06 +0530

    setup NextAuth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -8,7 +8,7 @@ export default NextAuth({
     adapter: PrismaAdapter(prisma),
     providers: [
         CredentialsProvider({
-            name: 'Credentials',
+            name: 'credentials',
             credentials: {
                 email: { label: 'Email', type: 'text' },
                 password: { label: 'Password', type: 'password' }
@@ -17,8 +17,33 @@ export default NextAuth({
                 if (!credentials?.email || !credentials?.password) {
                     throw new Error('Missing credentials');
                 }
+                const user = await prisma.user.findUnique({
+                    where: {
+                        email: credentials.email
+                    }
+                });
+                if (!user || !user?.hashedPassword) {
+                    throw new Error('User not found');
+                }   
+
+                const isCorrectPassword = await bcrypt.compare(credentials.password, user.hashedPassword);
+
+                if(!isCorrectPassword) {
+                    throw new Error('Incorrect password');
+                }
+
+                return user;
             }
         })
-    ]
+    ],
+    debug: process.env.NODE_ENV === 'development',
+    session: {
+        strategy: 'jwt',
+    },
+    jwt: {
+        secret: process.env.NEXTAUTH_JWT_SECRET,
+    },
+    secret: process.env.NEXTAUTH_SECRET,
+
 });
 
