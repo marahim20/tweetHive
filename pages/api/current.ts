5f12ee1352ede52e820deefe58f6fef9983d9012
Minhaj Abdul Rahim <minhajabdulraheem@gmail.com>
2023-03-19 22:47:38 +0530

    added pages and  hooks to handle comments, likes

--- pages/api/current.ts
+++ pages/api/current.ts
@@ -1,17 +1,18 @@
-import serverAuth from "@/libs/serverAuth";
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from 'next';
+
+import serverAuth from '@/libs/serverAuth';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method !== "GET") {
-        res.status(405).end();
-        return;
-    }
+  if (req.method !== 'GET') {
+    return res.status(405).end();
+  }
 
-    try {
-        const currentUser = await serverAuth(req);
-        return res.status(200).json(currentUser);
-    } catch (error) {
-        console.log(error);
-        return res.status(400).end();
-    }
-} 
+  try {
+    const { currentUser } = await serverAuth(req);
+
+    return res.status(200).json(currentUser);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).end();
+  }
+}
--- pages/api/edit.ts
+++ pages/api/edit.ts
@@ -4,35 +4,35 @@ import serverAuth from "@/libs/serverAuth";
 import prisma from "@/libs/prismadb";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method !== 'PATCH') {
-        return res.status(405).end();
+  if (req.method !== 'PATCH') {
+    return res.status(405).end();
+  }
+
+  try {
+    const { currentUser } = await serverAuth(req);
+
+    const { name, username, bio, profileImage, coverImage } = req.body;
+
+    if (!name || !username) {
+      throw new Error('Missing fields');
     }
 
-    try {
-        const { currentUser } = await serverAuth(req);
+    const updatedUser = await prisma.user.update({
+      where: {
+        id: currentUser.id,
+      },
+      data: {
+        name,
+        username,
+        bio,
+        profileImage,
+        coverImage
+      }
+    });
 
-        const { name, username, bio, profileImage, coverImage } = req.body;
-
-        if (!name || !username) {
-            throw new Error('Missing fields');
-        }
-
-        const updatedUser = await prisma.user.update({
-            where: {
-                id: currentUser.id,
-            },
-            data: {
-                name,
-                username,
-                bio,
-                profileImage,
-                coverImage
-            }
-        });
-
-        return res.status(200).json(updatedUser);
-    } catch (error) {
-        console.log(error);
-        return res.status(400).end();
-    }
+    return res.status(200).json(updatedUser);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).end();
+  }
 }
